dc9a2bc84c4ebb8f11a09f22ca9ff8cfe78bac95
tamjid <tamjidakash100@gmail.com>
2024-02-20 11:46:28 +0600

    ksadjfhsdkj

---
+++ Javascript/Condition.js
@@ -0,0 +1,77 @@
+// function farenheit(celcius){
+//   return celcius*(9/5)+32;
+// }
+// function area(r){
+//   return 3.1416*r**2;
+// }
+// function average2(a, b){
+//   return (a+b)/2;
+// }
+// function average3(a,b,c){
+//   return (a+b+c)/3;
+// }
+
+// function tax(sal){
+//   if(sal<=2000) {
+//     return "Without Taxes";
+//   }
+//   else if(sal>2000 && sal<=3000){
+//     return (sal*8)/100;
+//   }
+//   else if(sal>3000 && sal<=4500){
+//     return (sal*18)/100;
+//   } 
+//   else if(sal>4500){
+//     return (sal*28)/100;
+//   }
+// }
+// function age(year, month, day){
+//   return (year*365+month*30+day);
+// }
+// function time(h, m, s){
+//   return (h*60*60+m*60+s);
+// }
+// document.write(time(3,10,12));
+
+// function firstImage(){
+//   document.getElementById('im').src='download.jpeg';
+// }
+// function secondImage(){
+//   document.getElementById('im').src='flower.webp';
+// }
+//0=dlskjfs,1=5,2=ksjflksa,3=k;sajfl
+
+// let car = {brand:"Hino", color:"Red", price:30000};
+
+// let st = "This is "+car.brand+" car. Its color is "+car.color+". Its price is $"+car.price+".";
+// document.write(st);
+
+// function traverse(arr,len,lasjf){
+//  for(let i=0; i<len;i++){
+//   document.write(arr[i]+"<br>");
+//  }
+// }
+// let a = [5,6,10,3];
+// let b = [4,6,1,3];
+// let c= [1,2];
+
+// traverse(a,a.length);
+// traverse(b,b.length);
+// traverse(c,c.length);
+
+// function changeColor(){
+//  let col = document.getElementById('h').style.color;
+//  if(col =='green'){
+//  document.getElementById('h').style.color ="red";
+//  }
+//  else if(col=='red'){
+//   document.getElementById('h').style.color ="yellow";
+//  }
+//  else{
+//   document.getElementById('h').style.color ="green";
+//    }
+//  document.getElementById('h').style.fontSize ="50px";
+// }
+
+let a = "I am a \"programmer";
+document.write(a);
